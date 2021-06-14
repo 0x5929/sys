341497aae48f608e14d613621c54a0f122aba348
First Obj Oriented <work.kevin.ren@gmail.com>
2021-06-14 13:10:03 -0700

    Update procmon.sh

--- procmon.sh
+++ procmon.sh
@@ -14,7 +14,8 @@ while :
 do
         new=$(ps -eo command)
 
-        diff <(echo "$old") <(echo "$new") | egrep '[><]' | tee -a procmon.out
+        # diffing the old and the new processes, we only want the diff results denoted by ">" or "<" notation, and we are not interested in any kernel processes denoted by "[kworker...]"
+        diff <(echo "$old") <(echo "$new") | egrep '[><][^\[\]]'  | tee -a procmon.out
 
         sleep 1
         old=$new
