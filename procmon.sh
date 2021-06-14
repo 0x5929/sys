b7a12082d25e619193bf1f920776a4cd9073b54f
First Obj Oriented <work.kevin.ren@gmail.com>
2021-06-14 13:27:49 -0700

    Update procmon.sh

--- procmon.sh
+++ procmon.sh
@@ -13,17 +13,19 @@ echo "Procmon start time: $(TZ=":America/Los_Angeles" date)"
 while :
 do
         new=$(ps -eo command)
-
+        
         # diffing the old and the new processes, we only want the diff results denoted by ">" or "<" notation, and we are not interested in any kernel processes denoted by "[kworker...]"
         di=$(diff <(echo "$old") <(echo "$new") | egrep '[><][^\[\]]')
         if [ -z "$di" ]
         then
+                sleep 1
+                old=$new
                 continue
         else
 
                 echo "$di" "$(TZ=":America/Los_Angeles" date +%r)" | tee -a procmon.out
+                sleep 1
+                old=$new
         fi
 
-        sleep 1
-        old=$new
 done
